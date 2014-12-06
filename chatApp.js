c0bb3a7e1e1dd9b0e80fe1d127f0d461d1aacc0e
Bryan Richter <b@chreekat.net>
2014-12-06 19:59:13 +0100

    Reorganize chatApp.js

--- chatApp.js
+++ chatApp.js
@@ -1,33 +1,4 @@
-// Static data for now
-var channel = {
-    title: "Chan 1"
-};
-
-var messages = {};
-messages[0] = [
-    {
-        author: "Pancake",
-        date: "12:05",
-        message: "I am a message thing"
-    },
-    {
-        author: "FlappyHouse",
-        date: "13:08",
-        message: "You are not a message thing. I am. Note my message-ness."
-    },
-    {
-        author: "FrontPorch",
-        date: "13:10",
-        message: "Now, let me tell you whippersnappers about messages. Back in my day, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    }
-];
-// Lots of data to test scrolling.
-for (var i = 0; i < 4; i++) {
-    messages[0] = messages[0].concat(messages[0]);
-}
-
-
-// Here's where the UI is actually starts.
+// REACT COMPONENTS
 
 var ChatApp = React.createClass({
     render: function() {
@@ -60,7 +31,6 @@ var ChanSelector = React.createClass({
 });
 var ChatWindow = React.createClass({
     render: function() {
-        var msg1 = this.props.messages[0];
         var nodes = this.props.messages.map(function(msg, idx) {
             return (
                 <ChatMessage key={idx} {...msg} />
@@ -130,4 +100,35 @@ var ChanList = React.createClass({
     }
 });
 
+// STATIC TEST DATA
+
+var channel = {
+    title: "Chan 1"
+};
+
+var messages = {};
+messages[0] = [
+    {
+        author: "Pancake",
+        date: "12:05",
+        message: "I am a message thing"
+    },
+    {
+        author: "FlappyHouse",
+        date: "13:08",
+        message: "You are not a message thing. I am. Note my message-ness."
+    },
+    {
+        author: "FrontPorch",
+        date: "13:10",
+        message: "Now, let me tell you whippersnappers about messages. Back in my day, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    }
+];
+// Lots of data to test scrolling.
+for (var i = 0; i < 4; i++) {
+    messages[0] = messages[0].concat(messages[0]);
+}
+
+
+// FIRE ZE MISSILES
 React.render(<ChatApp />, document.getElementById("chatApp"));
