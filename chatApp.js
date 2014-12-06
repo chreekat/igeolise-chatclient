4874eed05def3e8d5339d2cd830aea0bf06c829b
Bryan Richter <b@chreekat.net>
2014-12-06 21:28:18 +0100

    Normalize model types with the server's output

--- chatApp.js
+++ chatApp.js
@@ -1,16 +1,22 @@
 // REACT COMPONENTS
 
 var ChanView = React.createClass({
+    getInitialState: function() {
+        return({
+            channels: this.props.initialChannels,
+            currentChannel: this.props.initialChannels[0]
+        });
+    },
     render: function() {
         return (
             <section className="chanView">
                 <header>
-                    <h2 className="chanView--title">{this.props.channel.title}</h2>
+                    <h2 className="chanView--title">{this.state.currentChannel.name}</h2>
                     <span className="chanView--chanSelect">
                         <ChanSelector />
                     </span>
                 </header>
-                <ChatWindow messages={messages[0]}/>
+                <ChatWindow messages={this.state.currentChannel.messages}/>
                 <textarea rows="3" className="chanView--chatInput" />
             </section>
         );
@@ -42,10 +48,10 @@ var ChatMessage = React.createClass({
         return (
             <section className="chatMessage">
                 <div className="chatMessage--meta">
-                    <div>{this.props.author}</div>
-                    <time className="chatMessage--date">{this.props.date}</time>
+                    <div>{this.props.user}</div>
+                    <time className="chatMessage--date">{this.props.stamp}</time>
                 </div>
-                <main className="chatMessage--data">{this.props.message}</main>
+                <main className="chatMessage--data">{this.props.text}</main>
             </section>
         );
     }
@@ -96,33 +102,34 @@ var ChanList = React.createClass({
 
 // STATIC TEST DATA
 
-var channel = {
-    title: "Chan 1"
-};
+var channels = [{
+    name: "Chan 1",
+    users: ['Bob', 'Frank'],
+    messages: []
+}];
 
-var messages = {};
-messages[0] = [
+channels[0].messages = channels[0].messages.concat([
     {
-        author: "Pancake",
-        date: "12:05",
-        message: "I am a message thing"
+        user: "Pancake",
+        stamp: "12:05",
+        text: "I am a message thing"
     },
     {
-        author: "FlappyHouse",
-        date: "13:08",
-        message: "You are not a message thing. I am. Note my message-ness."
+        user: "FlappyHouse",
+        stamp: "13:08",
+        text: "You are not a message thing. I am. Note my message-ness."
     },
     {
-        author: "FrontPorch",
-        date: "13:10",
-        message: "Now, let me tell you whippersnappers about messages. Back in my day, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        user: "FrontPorch",
+        stamp: "13:10",
+        text: "Now, let me tell you whippersnappers about messages. Back in my day, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
-];
+]);
 // Lots of data to test scrolling.
 for (var i = 0; i < 4; i++) {
-    messages[0] = messages[0].concat(messages[0]);
+    channels[0].messages = channels[0].messages.concat(channels[0].messages);
 }
 
 
 // FIRE ZE MISSILES
-React.render(<ChanView channel={channel}/>, document.getElementById("chatApp"));
+React.render(<ChanView initialChannels={channels}/>, document.getElementById("chatApp"));
